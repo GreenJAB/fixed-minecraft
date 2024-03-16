6d4c6ad0bc3515133131a2f589abf881567d4a59
joshua <joshua@baikie.org>
2024-03-16 11:01:52 +1100

    Blue/Packed/Ice melts in the nether

---
+++ src/main/java/net/greenjab/fixedminecraft/mixin/BlocksMixin.java
@@ -0,0 +1,39 @@
+package net.greenjab.fixedminecraft.mixin;
+
+import net.greenjab.fixedminecraft.blocks.BlueIceBlock;
+import net.greenjab.fixedminecraft.blocks.NewIceBlock;
+import net.greenjab.fixedminecraft.blocks.PackedIceBlock;
+import net.minecraft.block.AbstractBlock;
+import net.minecraft.block.Block;
+import net.minecraft.block.Blocks;
+
+import net.minecraft.block.IceBlock;
+import net.minecraft.block.TranslucentBlock;
+import org.spongepowered.asm.mixin.Mixin;
+import org.spongepowered.asm.mixin.injection.At;
+import org.spongepowered.asm.mixin.injection.Redirect;
+import org.spongepowered.asm.mixin.injection.Slice;
+
+@Mixin(Blocks.class)
+public class BlocksMixin {
+
+    @Redirect(slice = @Slice(from = @At(value = "CONSTANT",args= {
+            "stringValue=ice"},ordinal = 0)),at = @At(
+            value = "NEW",target = "Lnet/minecraft/block/IceBlock;*", ordinal = 0 ),method = "<clinit>")
+    private static IceBlock Ice(AbstractBlock.Settings settings) {
+        return new NewIceBlock(settings);
+    }
+    @Redirect(slice = @Slice(from = @At(value = "CONSTANT",args= {
+            "stringValue=packed_ice"},ordinal = 0)),at = @At(
+            value = "NEW",target = "Lnet/minecraft/block/Block;*", ordinal = 0 ),method = "<clinit>")
+    private static Block packedIce(AbstractBlock.Settings settings) {
+        return new PackedIceBlock(settings.ticksRandomly());
+    }
+    @Redirect(slice = @Slice(from = @At(value = "CONSTANT",args= {
+            "stringValue=blue_ice"},ordinal = 0)),at = @At(
+                    value = "NEW",target = "Lnet/minecraft/block/TranslucentBlock;*", ordinal = 0 ),method = "<clinit>")
+    private static TranslucentBlock blueIce(AbstractBlock.Settings settings) {
+        return new BlueIceBlock(settings.ticksRandomly());
+    }
+
+}
---
+++ src/main/kotlin/net/greenjab/fixedminecraft/blocks/PackedIceBlock.kt
@@ -0,0 +1,50 @@
+package net.greenjab.fixedminecraft.blocks;
+
+import com.mojang.serialization.MapCodec
+import net.minecraft.block.BlockState
+import net.minecraft.block.Blocks
+import net.minecraft.block.TranslucentBlock
+import net.minecraft.block.entity.BlockEntity
+import net.minecraft.enchantment.EnchantmentHelper
+import net.minecraft.enchantment.Enchantments
+import net.minecraft.entity.player.PlayerEntity
+import net.minecraft.item.ItemStack
+import net.minecraft.server.world.ServerWorld
+import net.minecraft.util.math.BlockPos
+import net.minecraft.util.math.random.Random
+import net.minecraft.world.World
+import org.jetbrains.annotations.Nullable
+
+class PackedIceBlock (settings: Settings) : TranslucentBlock(settings) {
+
+    public override fun getCodec() = CODEC
+
+    companion object {
+        val CODEC: MapCodec<PackedIceBlock> = createCodec { settings: Settings -> PackedIceBlock(settings) }
+    }
+
+	private fun getMeltedState():BlockState {
+		return Blocks.ICE.getDefaultState();
+	}
+
+    override fun afterBreak(world:World, player: PlayerEntity, pos:BlockPos, state:BlockState, blockEntity: BlockEntity?, tool: ItemStack) {
+        super.afterBreak(world, player, pos, state, blockEntity, tool);
+		if (EnchantmentHelper.getLevel(Enchantments.SILK_TOUCH, tool) == 0) {
+			if (world.getDimension().ultrawarm()) {
+				world.removeBlock(pos, false);
+				return;
+			}
+		}
+	}
+
+	override fun randomTick(state:BlockState, world:ServerWorld, pos:BlockPos, random:Random) {
+		if (world.getDimension().ultrawarm()) {
+			this.melt(state, world, pos);
+		}
+	}
+
+	private fun melt(state:BlockState, world:World, pos:BlockPos) {
+        world.setBlockState(pos, getMeltedState());
+        world.updateNeighbor(pos, getMeltedState().getBlock(), pos);
+	}
+}
