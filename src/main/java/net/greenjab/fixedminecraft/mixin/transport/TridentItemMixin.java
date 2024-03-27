bfc613cd4702837fb25020d8751c13b26e72d8f6
Kikugie <kikugie@duck.com>
2024-03-27 01:28:49 +0100

    Buff only horizontal riptide velocity

--- src/main/java/net/greenjab/fixedminecraft/mixin/transport/TridentItemMixin.java
+++ src/main/java/net/greenjab/fixedminecraft/mixin/transport/TridentItemMixin.java
@@ -1,25 +1,29 @@
 package net.greenjab.fixedminecraft.mixin.transport;
 
-import com.llamalad7.mixinextras.sugar.Local;
-import com.llamalad7.mixinextras.sugar.ref.LocalIntRef;
-import net.minecraft.entity.LivingEntity;
-import net.minecraft.item.ItemStack;
+import com.llamalad7.mixinextras.injector.wrapoperation.Operation;
+import com.llamalad7.mixinextras.injector.wrapoperation.WrapOperation;
+import net.minecraft.entity.player.PlayerEntity;
 import net.minecraft.item.TridentItem;
-import net.minecraft.world.World;
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.injection.At;
-import org.spongepowered.asm.mixin.injection.Inject;
-import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
 
 /**
  * Boosts riptide efficiency when used from water (balancing its extreme effectiveness during rain in combination with elytra)
  */
 @Mixin(TridentItem.class)
 public class TridentItemMixin {
-    @Inject(method = "onStoppedUsing", at = @At(value = "INVOKE", target = "Lnet/minecraft/util/math/MathHelper;sqrt(F)F"))
-    private void modifyRiptideStrength(ItemStack stack, World world, LivingEntity user, int remainingUseTicks, CallbackInfo ci, @Local(ordinal = 1) LocalIntRef riptide) {
-        // TODO: Configure scale via config system
-        // TODO: Move the injection point to assigned `n` to have float scaling, but how tho
-        if (user.isTouchingWater()) riptide.set(riptide.get() * 2);
+    @WrapOperation(
+            method = "onStoppedUsing", at = @At(
+            value = "INVOKE",
+            target = "Lnet/minecraft/entity/player/PlayerEntity;addVelocity(DDD)V"
+    )
+    )
+    private void modifyRiptideStrength(PlayerEntity player, double x, double y, double z, Operation<Void> original) {
+        //TODO configuration
+        if (player.isTouchingWater()) {
+            x *= 3F;
+            z *= 3F;
+        }
+        original.call(player, x, y, z);
     }
 }
