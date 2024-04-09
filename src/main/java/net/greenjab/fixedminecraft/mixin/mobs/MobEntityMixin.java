8de3c789b77d80b8aa3366c9c31195c9fb3d6652
laazuli <laazuli.laazuli@gmail.com>
2024-04-10 01:10:38 +0200

    Feature: mobs have a chance to spawn with randomly trimmed armor

---
+++ src/main/java/net/greenjab/fixedminecraft/mixin/mobs/MobEntityMixin.java
@@ -0,0 +1,25 @@
+package net.greenjab.fixedminecraft.mixin.mobs;
+
+import net.greenjab.fixedminecraft.util.mobs.ArmorTrimmer;
+import net.minecraft.entity.EntityType;
+import net.minecraft.entity.LivingEntity;
+import net.minecraft.entity.mob.MobEntity;
+import net.minecraft.item.ItemStack;
+import net.minecraft.world.World;
+import org.spongepowered.asm.mixin.Mixin;
+import org.spongepowered.asm.mixin.injection.At;
+import org.spongepowered.asm.mixin.injection.ModifyArg;
+
+@Mixin(MobEntity.class)
+public abstract class MobEntityMixin extends LivingEntity {
+
+    protected MobEntityMixin(EntityType<? extends LivingEntity> entityType, World world) {
+        super(entityType, world);
+    }
+
+    // Belongs to Feature: Mobs have a chance to spawn with randomly trimmed armor
+    @ModifyArg(method = "initEquipment", at = @At(value = "INVOKE", target = "Lnet/minecraft/entity/mob/MobEntity;equipStack(Lnet/minecraft/entity/EquipmentSlot;Lnet/minecraft/item/ItemStack;)V"), index = 1)
+    public ItemStack trimArmor(ItemStack stack) {
+        return ArmorTrimmer.trimAtChanceIfTrimable(stack, this.random, this.getWorld().getRegistryManager());
+    }
+}
---
+++ src/main/java/net/greenjab/fixedminecraft/mixin/mobs/PiglinEntityMixin.java
@@ -0,0 +1,24 @@
+package net.greenjab.fixedminecraft.mixin.mobs;
+
+import net.greenjab.fixedminecraft.util.mobs.ArmorTrimmer;
+import net.minecraft.entity.EntityType;
+import net.minecraft.entity.mob.AbstractPiglinEntity;
+import net.minecraft.entity.mob.PiglinEntity;
+import net.minecraft.item.ItemStack;
+import net.minecraft.world.World;
+import org.spongepowered.asm.mixin.Mixin;
+import org.spongepowered.asm.mixin.injection.At;
+import org.spongepowered.asm.mixin.injection.ModifyArg;
+
+@Mixin(PiglinEntity.class)
+public abstract class PiglinEntityMixin extends AbstractPiglinEntity {
+    public PiglinEntityMixin(EntityType<? extends AbstractPiglinEntity> entityType, World world) {
+        super(entityType, world);
+    }
+
+    // Belongs to Feature: Mobs have a chance to spawn with randomly trimmed armor
+    @ModifyArg(method = "initEquipment", at = @At(value = "INVOKE", target = "Lnet/minecraft/entity/mob/PiglinEntity;equipAtChance(Lnet/minecraft/entity/EquipmentSlot;Lnet/minecraft/item/ItemStack;Lnet/minecraft/util/math/random/Random;)V"), index = 1)
+    public ItemStack trimAtChance(ItemStack stack) {
+        return ArmorTrimmer.trimAtChanceIfTrimable(stack, this.random, this.getWorld().getRegistryManager());
+    }
+}
