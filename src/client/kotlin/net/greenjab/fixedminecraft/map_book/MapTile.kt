9af23bc31323311a3ff322b7c0356c75025dd189
Nettakrim <106409257+Nettakrim@users.noreply.github.com>
2024-04-23 22:15:10 +0100

    holding a map now shows you the most relevant nearest map, fixed icons not showing up (now theyre just weird), maps in map books now update and are sent to the client properly

--- src/client/kotlin/net/greenjab/fixedminecraft/map_book/MapTile.kt
+++ src/client/kotlin/net/greenjab/fixedminecraft/map_book/MapTile.kt
@@ -16,10 +16,12 @@ class MapTile(var screen: MapBookScreen, var id: Int, var mapState: MapState, va
 
         context.matrices.push()
 
+        // the z scale and translating ensures larger maps render behind smaller ones, but it messes up when markers are on the border of a map
+        // it should be possible to have the best of both worlds by increasing the -1.0 scale, but its not worth doing the maths yet
         context.matrices.translate(screen.x, screen.y, (1.0/(mapState.scale.toInt()+1.0))+1.0)
-        context.matrices.scale(screen.scale, screen.scale, 1.0f)
+        context.matrices.scale(screen.scale, screen.scale, -1.0f)
 
-        context.matrices.translate(mapState.centerX.toDouble() - offset + context.scaledWindowWidth/2, mapState.centerZ.toDouble() - offset + context.scaledWindowHeight/2,1.0)
+        context.matrices.translate(mapState.centerX.toDouble() - offset + context.scaledWindowWidth/2, mapState.centerZ.toDouble() - offset + context.scaledWindowHeight/2,0.0)
         context.matrices.scale(mapScale, mapScale, 1.0f)
 
         client.gameRenderer.mapRenderer.draw(context.matrices, context.vertexConsumers, id, mapState, false, LightmapTextureManager.MAX_LIGHT_COORDINATE)
