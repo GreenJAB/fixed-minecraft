a6256b99b3c4e30ffb138d5ce1070d1c8d46748a
Kikugie <kikugie@duck.com>
2024-05-10 19:13:25 +0200

    Make the copper rail oxidize

---
+++ src/main/java/net/greenjab/fixedminecraft/mixin/transport/OxidizableMixin.java
@@ -0,0 +1,26 @@
+package net.greenjab.fixedminecraft.mixin.transport;
+
+import com.google.common.collect.ImmutableBiMap;
+import com.llamalad7.mixinextras.injector.ModifyExpressionValue;
+import net.greenjab.fixedminecraft.registry.BlockRegistry;
+import net.minecraft.block.Block;
+import net.minecraft.block.Oxidizable;
+import org.spongepowered.asm.mixin.Mixin;
+import org.spongepowered.asm.mixin.injection.At;
+
+@Mixin(Oxidizable.class)
+public interface OxidizableMixin {
+    @ModifyExpressionValue(
+            method = "method_34740", at = @At(
+            value = "INVOKE",
+            target = "Lcom/google/common/collect/ImmutableBiMap;builder()Lcom/google/common/collect/ImmutableBiMap$Builder;",
+            ordinal = 0
+    )
+    )
+    private static ImmutableBiMap.Builder<Block, Block> addCopperRails(ImmutableBiMap.Builder<Block, Block> original) {
+        return original
+                .put(BlockRegistry.COPPER_RAIL, BlockRegistry.EXPOSED_COPPER_RAIL)
+                .put(BlockRegistry.EXPOSED_COPPER_RAIL, BlockRegistry.WEATHERED_COPPER_RAIL)
+                .put(BlockRegistry.WEATHERED_COPPER_RAIL, BlockRegistry.OXIDIZED_COPPER_RAIL);
+    }
+}
--- src/main/kotlin/net/greenjab/fixedminecraft/registry/BlockRegistry.kt
+++ src/main/kotlin/net/greenjab/fixedminecraft/registry/BlockRegistry.kt
@@ -25,15 +25,15 @@ object BlockRegistry {
         sounds(BlockSoundGroup.ANVIL)
         pistonBehavior(PistonBehavior.BLOCK)
     }
-    val COPPER_RAIL = block(Blocks.POWERED_RAIL, { OxidizableRailBlock(Oxidizable.OxidationLevel.UNAFFECTED, it) })
-    val EXPOSED_COPPER_RAIL = block(Blocks.POWERED_RAIL, { OxidizableRailBlock(Oxidizable.OxidationLevel.EXPOSED, it) })
-    val WEATHERED_COPPER_RAIL = block(Blocks.POWERED_RAIL, { OxidizableRailBlock(Oxidizable.OxidationLevel.WEATHERED, it) })
-    val OXIDIZED_COPPER_RAIL = block(Blocks.POWERED_RAIL, { OxidizableRailBlock(Oxidizable.OxidationLevel.OXIDIZED, it) })
+    @JvmField val COPPER_RAIL = block(Blocks.POWERED_RAIL, { OxidizableRailBlock(Oxidizable.OxidationLevel.UNAFFECTED, it) })
+    @JvmField val EXPOSED_COPPER_RAIL = block(Blocks.POWERED_RAIL, { OxidizableRailBlock(Oxidizable.OxidationLevel.EXPOSED, it) })
+    @JvmField val WEATHERED_COPPER_RAIL = block(Blocks.POWERED_RAIL, { OxidizableRailBlock(Oxidizable.OxidationLevel.WEATHERED, it) })
+    @JvmField val OXIDIZED_COPPER_RAIL = block(Blocks.POWERED_RAIL, { OxidizableRailBlock(Oxidizable.OxidationLevel.OXIDIZED, it) })
 
-    val WAXED_COPPER_RAIL = block(Blocks.POWERED_RAIL, ::CopperRailBlock)
-    val WAXED_EXPOSED_COPPER_RAIL = block(Blocks.POWERED_RAIL, ::CopperRailBlock)
-    val WAXED_WEATHERED_COPPER_RAIL = block(Blocks.POWERED_RAIL, ::CopperRailBlock)
-    val WAXED_OXIDIZED_COPPER_RAIL = block(Blocks.POWERED_RAIL, ::CopperRailBlock)
+    @JvmField val WAXED_COPPER_RAIL = block(Blocks.POWERED_RAIL, ::CopperRailBlock)
+    @JvmField val WAXED_EXPOSED_COPPER_RAIL = block(Blocks.POWERED_RAIL, ::CopperRailBlock)
+    @JvmField val WAXED_WEATHERED_COPPER_RAIL = block(Blocks.POWERED_RAIL, ::CopperRailBlock)
+    @JvmField val WAXED_OXIDIZED_COPPER_RAIL = block(Blocks.POWERED_RAIL, ::CopperRailBlock)
 
     fun register() {
         BLOCK.register("netherite_anvil", NETHERITE_ANVIL)
