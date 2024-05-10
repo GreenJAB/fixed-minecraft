448717a6a0cf54d711c82c3dbe6998ac9fe73885
Kikugie <kikugie@duck.com>
2024-05-10 19:14:25 +0200

    Add copper rails to the render layer

--- src/client/kotlin/net/greenjab/fixedminecraft/FixedMinecraftClient.kt
+++ src/client/kotlin/net/greenjab/fixedminecraft/FixedMinecraftClient.kt
@@ -1,10 +1,25 @@
 package net.greenjab.fixedminecraft
 
 import net.fabricmc.api.ClientModInitializer
+import net.fabricmc.fabric.api.blockrenderlayer.v1.BlockRenderLayerMap
 import net.greenjab.fixedminecraft.network.ClientSyncHandler
+import net.greenjab.fixedminecraft.registry.BlockRegistry
+import net.minecraft.client.render.RenderLayer
 
 object FixedMinecraftClient : ClientModInitializer {
     override fun onInitializeClient() {
         ClientSyncHandler.init()
+
+        BlockRenderLayerMap.INSTANCE.putBlocks(
+            RenderLayer.getCutout(),
+            BlockRegistry.COPPER_RAIL,
+            BlockRegistry.EXPOSED_COPPER_RAIL,
+            BlockRegistry.WEATHERED_COPPER_RAIL,
+            BlockRegistry.OXIDIZED_COPPER_RAIL,
+            BlockRegistry.WAXED_COPPER_RAIL,
+            BlockRegistry.WAXED_EXPOSED_COPPER_RAIL,
+            BlockRegistry.WAXED_WEATHERED_COPPER_RAIL,
+            BlockRegistry.WAXED_OXIDIZED_COPPER_RAIL
+        )
     }
 }
