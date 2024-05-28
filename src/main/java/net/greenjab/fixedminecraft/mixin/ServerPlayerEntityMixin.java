cba860d4075466947c461fc18492d8f6494140b0
joshua <joshua@baikie.org>
2024-05-28 23:40:17 +1000

    Armour saturation drain is based on type rather than overall protection leather-chain no effect, gold-iron medium effect, diamond-netherite large effect, max drains 1.5 times faster

--- src/main/java/net/greenjab/fixedminecraft/mixin/ServerPlayerEntityMixin.java
+++ src/main/java/net/greenjab/fixedminecraft/mixin/ServerPlayerEntityMixin.java
@@ -3,6 +3,7 @@ package net.greenjab.fixedminecraft.mixin;
 
 import net.minecraft.entity.Entity;
 import net.minecraft.entity.EntityType;
+import net.minecraft.item.ItemStack;
 import net.minecraft.server.network.ServerPlayerEntity;
 import net.minecraft.world.World;
 import org.spongepowered.asm.mixin.Mixin;
@@ -13,6 +14,8 @@ import org.spongepowered.asm.mixin.injection.Inject;
 import org.spongepowered.asm.mixin.injection.ModifyConstant;
 import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
 import net.greenjab.fixedminecraft.network.SyncHandler;
+
+import java.util.Iterator;
 
 @Mixin(ServerPlayerEntity.class)
 public abstract class ServerPlayerEntityMixin extends Entity
@@ -33,6 +36,13 @@ public abstract class ServerPlayerEntityMixin extends Entity
     }
     @ModifyConstant(method = "increaseTravelMotionStats", constant = @Constant(floatValue = 0.1f))
     public float armorDrainsStamina(float constant) {
-        return 0.1f*((this.lastArmorScore/20.0f)+1.0f);
+        int weight = 0;
+        Iterator<ItemStack> e = this.getArmorItems().iterator();
+        for (int i = 0;i<4;i++) {
+            String s = e.next().getName().toString();
+            if (s.contains("iron")||s.contains("gold")) weight+=1;
+            if (s.contains("diamond")||s.contains("netherite")) weight+=2;
+        }
+        return 0.1f*((weight/16.0f)+1.0f);
     }
 }
