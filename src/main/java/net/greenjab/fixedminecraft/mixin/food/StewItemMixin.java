e907c5222212fac1c81be380b314a6bf9f98fdb5
joshua <joshua@baikie.org>
2024-06-15 23:59:56 +1000

    Added 2x2 crafting
    and moved some food files

--- src/main/java/net/greenjab/fixedminecraft/mixin/food/StewItemMixin.java
+++ src/main/java/net/greenjab/fixedminecraft/mixin/food/StewItemMixin.java
@@ -1,11 +1,13 @@
-package net.greenjab.fixedminecraft.mixin;
+package net.greenjab.fixedminecraft.mixin.food;
 
 import net.minecraft.advancement.criterion.Criteria;
 import net.minecraft.entity.LivingEntity;
+import net.minecraft.entity.effect.StatusEffects;
 import net.minecraft.entity.player.PlayerEntity;
+import net.minecraft.item.Item;
 import net.minecraft.item.ItemStack;
 import net.minecraft.item.Items;
-import net.minecraft.item.SuspiciousStewItem;
+import net.minecraft.item.StewItem;
 import net.minecraft.server.network.ServerPlayerEntity;
 import net.minecraft.stat.Stats;
 import net.minecraft.world.World;
@@ -13,15 +15,16 @@ import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.injection.At;
 import org.spongepowered.asm.mixin.injection.Inject;
 import org.spongepowered.asm.mixin.injection.callback.CallbackInfoReturnable;
+import org.spongepowered.asm.mixin.injection.callback.LocalCapture;
 
-@Mixin(SuspiciousStewItem.class)
-public class SuspiciousStewItemMixin {
+@Mixin(StewItem.class)
+public class StewItemMixin {
 
     @Inject(method = "finishUsing", at = @At("TAIL"), cancellable = true)
     private void fixStackedBowlUse(ItemStack stack, World world, LivingEntity user, CallbackInfoReturnable cir) {
         if (user instanceof ServerPlayerEntity serverPlayerEntity) {
             Criteria.CONSUME_ITEM.trigger(serverPlayerEntity, stack);
-            serverPlayerEntity.incrementStat(Stats.USED.getOrCreateStat((SuspiciousStewItem)(Object)this));
+            serverPlayerEntity.incrementStat(Stats.USED.getOrCreateStat((StewItem)(Object)this));
         }
         if (stack.isEmpty()) {
             cir.setReturnValue( new ItemStack(Items.BOWL));
--- src/main/java/net/greenjab/fixedminecraft/mixin/food/SuspiciousStewItemMixin.java
+++ src/main/java/net/greenjab/fixedminecraft/mixin/food/SuspiciousStewItemMixin.java
@@ -1,13 +1,11 @@
-package net.greenjab.fixedminecraft.mixin;
+package net.greenjab.fixedminecraft.mixin.food;
 
 import net.minecraft.advancement.criterion.Criteria;
 import net.minecraft.entity.LivingEntity;
-import net.minecraft.entity.effect.StatusEffects;
 import net.minecraft.entity.player.PlayerEntity;
-import net.minecraft.item.Item;
 import net.minecraft.item.ItemStack;
 import net.minecraft.item.Items;
-import net.minecraft.item.StewItem;
+import net.minecraft.item.SuspiciousStewItem;
 import net.minecraft.server.network.ServerPlayerEntity;
 import net.minecraft.stat.Stats;
 import net.minecraft.world.World;
@@ -15,16 +13,15 @@ import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.injection.At;
 import org.spongepowered.asm.mixin.injection.Inject;
 import org.spongepowered.asm.mixin.injection.callback.CallbackInfoReturnable;
-import org.spongepowered.asm.mixin.injection.callback.LocalCapture;
 
-@Mixin(StewItem.class)
-public class StewItemMixin {
+@Mixin(SuspiciousStewItem.class)
+public class SuspiciousStewItemMixin {
 
     @Inject(method = "finishUsing", at = @At("TAIL"), cancellable = true)
     private void fixStackedBowlUse(ItemStack stack, World world, LivingEntity user, CallbackInfoReturnable cir) {
         if (user instanceof ServerPlayerEntity serverPlayerEntity) {
             Criteria.CONSUME_ITEM.trigger(serverPlayerEntity, stack);
-            serverPlayerEntity.incrementStat(Stats.USED.getOrCreateStat((StewItem)(Object)this));
+            serverPlayerEntity.incrementStat(Stats.USED.getOrCreateStat((SuspiciousStewItem)(Object)this));
         }
         if (stack.isEmpty()) {
             cir.setReturnValue( new ItemStack(Items.BOWL));
