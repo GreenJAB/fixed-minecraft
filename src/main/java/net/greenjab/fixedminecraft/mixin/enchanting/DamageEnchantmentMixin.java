3b0e11eefdd9b34ea98cf8d5579ef5dedef68a9e
joshua <joshua@baikie.org>
2024-06-19 20:41:32 +1000

    mapbooks can interact with banners

---
+++ src/main/java/net/greenjab/fixedminecraft/mixin/enchanting/DamageEnchantmentMixin.java
@@ -0,0 +1,17 @@
+package net.greenjab.fixedminecraft.mixin.enchanting;
+
+import net.minecraft.enchantment.DamageEnchantment;
+import net.minecraft.enchantment.Enchantment;
+import org.spongepowered.asm.mixin.Mixin;
+import org.spongepowered.asm.mixin.injection.At;
+import org.spongepowered.asm.mixin.injection.Inject;
+import org.spongepowered.asm.mixin.injection.callback.CallbackInfoReturnable;
+
+@Mixin(DamageEnchantment.class)
+public class DamageEnchantmentMixin {
+
+    @Inject(method = "canAccept", at = @At("HEAD"), cancellable = true)
+    private void removeExclusivity(Enchantment other, CallbackInfoReturnable<Boolean> cir) {
+        cir.setReturnValue((DamageEnchantment)(Object)this != other);
+    }
+}
--- src/main/kotlin/net/greenjab/fixedminecraft/items/map_book/MapBookItem.kt
+++ src/main/kotlin/net/greenjab/fixedminecraft/items/map_book/MapBookItem.kt
@@ -6,16 +6,19 @@ import net.minecraft.entity.Entity
 import net.minecraft.entity.player.PlayerEntity
 import net.minecraft.item.FilledMapItem
 import net.minecraft.item.ItemStack
+import net.minecraft.item.ItemUsageContext
 import net.minecraft.item.Items
 import net.minecraft.item.NetworkSyncedItem
 import net.minecraft.item.map.MapState
 import net.minecraft.nbt.NbtElement
+import net.minecraft.registry.tag.BlockTags
 import net.minecraft.screen.ScreenTexts
 import net.minecraft.server.MinecraftServer
 import net.minecraft.server.network.ServerPlayerEntity
 import net.minecraft.server.world.ServerWorld
 import net.minecraft.sound.SoundEvents
 import net.minecraft.text.Text
+import net.minecraft.util.ActionResult
 import net.minecraft.util.Formatting
 import net.minecraft.util.Hand
 import net.minecraft.util.TypedActionResult
@@ -27,6 +30,21 @@ import kotlin.math.max
 
 class MapBookItem(settings: Settings?) : NetworkSyncedItem(settings) {
     private val MAP_BOOK_KEY = "fixedminecraft:map_book"
+
+    override fun useOnBlock(context: ItemUsageContext): ActionResult {
+        val blockState = context.world.getBlockState(context.blockPos)
+        if (blockState.isIn(BlockTags.BANNERS)) {
+            if (!context.world.isClient) {
+                val mapState = getNearestMap(context.stack, context.world, context.blockPos.toCenterPos())?.mapState
+                if (mapState != null && !mapState.addBanner(context.world, context.blockPos)) {
+                    return ActionResult.FAIL
+                }
+            }
+            return ActionResult.success(context.world.isClient)
+        } else {
+            return super.useOnBlock(context)
+        }
+    }
 
     override fun use(world: World?, user: PlayerEntity?, hand: Hand?): TypedActionResult<ItemStack> {
         if (world != null && !world.isClient()) {
@@ -102,7 +120,7 @@ class MapBookItem(settings: Settings?) : NetworkSyncedItem(settings) {
     }
 
     fun getNearestMap(stack: ItemStack, world: World, pos: Vec3d): MapStateData? {
-        //get nearest map, if inside multiple maps, choose smallest resolution
+        // get nearest map, if inside multiple maps, choose smallest resolution
         var nearestDistance = Double.MAX_VALUE
         var nearestScale: Byte = Byte.MAX_VALUE
         var nearestMap: MapStateData? = null
@@ -113,7 +131,7 @@ class MapBookItem(settings: Settings?) : NetworkSyncedItem(settings) {
 
             val roughlyEqual = abs(nearestDistance-distance) < 1
             if (distance < nearestDistance || roughlyEqual) {
-                //if two maps are tied, take the smaller one if inside, or the larger one if outside
+                // if two maps are tied, take the smaller one if inside, or the larger one if outside
                 if (!roughlyEqual || (distance < 0 && mapStateData.mapState.scale < nearestScale) || (distance > 0 && mapStateData.mapState.scale > nearestScale)) {
                     nearestDistance = distance
                     nearestScale = mapStateData.mapState.scale
@@ -170,7 +188,7 @@ class MapBookItem(settings: Settings?) : NetworkSyncedItem(settings) {
         val state = getOrCreateMapBookState(item, world.server)
 
         val nearestState = getNearestMap(item, world, pos)
-        //make a new map if the book has no maps, the position is outside a map, or the map the position is in has a larger scale
+        // make a new map if the book has no maps, the position is outside a map, or the map the position is in has a larger scale
         if (nearestState == null || nearestState.mapState.scale > scale || getDistanceToEdgeOfMap(nearestState.mapState, pos) > 0) {
             val newMap = FilledMapItem.createMap(world, floor(pos.x).toInt(), floor(pos.z).toInt(), scale.toByte(), true, false)
             state.addMapID(FilledMapItem.getMapId(newMap)!!)
