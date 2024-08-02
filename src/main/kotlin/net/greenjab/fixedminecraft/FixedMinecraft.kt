16755f11e16f6cf4f99b98c412a88d8adf96e1ef
Nettakrim <106409257+Nettakrim@users.noreply.github.com>
2024-08-02 13:02:55 +0100

    added partially functional map book cloning and changed occurrences of fixed_minecraft to fixedminecraft

--- src/main/kotlin/net/greenjab/fixedminecraft/FixedMinecraft.kt
+++ src/main/kotlin/net/greenjab/fixedminecraft/FixedMinecraft.kt
@@ -6,11 +6,12 @@ import net.greenjab.fixedminecraft.StatusEffects.StatusRegistry
 import net.greenjab.fixedminecraft.registry.BlockRegistry
 import net.greenjab.fixedminecraft.registry.ItemGroupRegistry
 import net.greenjab.fixedminecraft.registry.ItemRegistry
+import net.greenjab.fixedminecraft.registry.RecipeRegistry
 import org.slf4j.LoggerFactory
 
 
 object FixedMinecraft : ModInitializer {
-    private val logger = LoggerFactory.getLogger("FixedMinecraft")
+    val logger = LoggerFactory.getLogger("FixedMinecraft")
 
     override fun onInitialize() {
         logger.info("Initializing ${FixedMinecraftConstants.MOD_NAME}")
@@ -22,8 +23,9 @@ object FixedMinecraft : ModInitializer {
 
         BlockRegistry.register()
         ItemRegistry.register()
-        StatusRegistry.register()
 
         ItemGroupRegistry.register()
+
+        RecipeRegistry.register()
     }
 }
