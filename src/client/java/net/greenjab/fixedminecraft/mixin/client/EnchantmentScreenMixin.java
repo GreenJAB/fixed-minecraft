fcfca25ceba651b6a66c2bee5d87b9ecac800ca9
Izwak <isaac.bullock@outlook.com>
2024-09-10 18:52:05 +1000

    Visually shows lapis needed (upto 3) and properly unlocks buttons appropriately

--- src/client/java/net/greenjab/fixedminecraft/mixin/client/EnchantmentScreenMixin.java
+++ src/client/java/net/greenjab/fixedminecraft/mixin/client/EnchantmentScreenMixin.java
@@ -6,6 +6,8 @@ import net.minecraft.screen.EnchantmentScreenHandler;
 import net.minecraft.util.Identifier;
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.injection.At;
+import org.spongepowered.asm.mixin.injection.Constant;
+import org.spongepowered.asm.mixin.injection.ModifyConstant;
 import org.spongepowered.asm.mixin.injection.ModifyVariable;
 import org.spongepowered.asm.mixin.injection.Redirect;
 
@@ -16,34 +18,30 @@ public class EnchantmentScreenMixin {
 
     @ModifyVariable(method = "render", at = @At("STORE"), ordinal = 6)
     private int lapiscost(int m, @Local(ordinal = 4) int power) {
-        //System.out.print(power);
         return (int)Math.ceil(power/10.0);
     }
 
-    //TODO 102: (k < l + 1) > Need to make button activate when there is enough lapis for that button, not just 123
+    @ModifyConstant(method = "drawBackground", constant = @Constant(intValue = 1, ordinal = 0))
+    private int injected(int i, @Local(ordinal = 5) int l, @Local(ordinal = 8) int power) {
+        return (int)Math.ceil(power/10.0)-l;
+    }
 
-    /*@Redirect(method = "drawBackground", at = @At(value = "FIELD", target = "L", opcode = Opcodes.GETFIELD))
-    private int injected(ClientPlayerEntity instance) {
-        return 12345;
-    }*/
-
-    //TODO 117: LEVEL_TEXTURES[l] > make the 123 icons based on (0-0.5)(0.5-1)(1-) of enchant capacity, not just 123
-
-    /*@Redirect(method = "drawBackground", at = @At(value = "FIELD", target = "Lnet/minecraft/client/gui/screen/ingame/EnchantmentScreen;LEVEL_TEXTURES:[Lnet/minecraft/util/Identifier;"))
-    private Identifier[] injected(@Local(ordinal = 6)int power) {
+    @Redirect(method = "drawBackground", at = @At(value = "FIELD", target = "Lnet/minecraft/client/gui/screen/ingame/EnchantmentScreen;LEVEL_TEXTURES:[Lnet/minecraft/util/Identifier;"))
+    private Identifier[] iconTextures(@Local(ordinal = 8) int power) {
         Identifier[] TEXTURES = new Identifier[]{new Identifier("container/enchanting_table/level_1"), new Identifier("container/enchanting_table/level_2"), new Identifier("container/enchanting_table/level_3")};
-        EnchantmentScreen screen = (EnchantmentScreen)(Object)this;
-        //((EnchantmentScreenHandler)screen.handler)
-        return TEXTURES;
-    }*/
+        power = Math.min((int)Math.ceil(power/10.0)-1, 2);
+        Identifier[] TEXTURES_REORDERED = new Identifier[3];
+        for (int i = 0;i<3;i++) TEXTURES_REORDERED[i] = TEXTURES[power];
+        return TEXTURES_REORDERED;
+    }
 
-    /*@Redirect(method = "drawBackground", slice = @Slice(from = @At(
-            value = "FIELD", args = {
-            "stringValue=phantom_membrane"}, ordinal = 0, target = ""
-    )), at = @At(
-            value = "NEW",target = "(Lnet/minecraft/item/Item$Settings;)Lnet/minecraft/item/Item;", ordinal = 0 ))
-    private Item edibleMembrane(Item.Settings settings) {
-        return new PhantomMembraneItem((new Item.Settings()).maxCount(64).food(FoodComponents.CHORUS_FRUIT));
-    }//*/
+    @Redirect(method = "drawBackground", at = @At(value = "FIELD", target = "Lnet/minecraft/client/gui/screen/ingame/EnchantmentScreen;LEVEL_DISABLED_TEXTURES:[Lnet/minecraft/util/Identifier;"))
+    private Identifier[] iconTexturesDisabled(@Local(ordinal = 8) int power) {
+        Identifier[] TEXTURES = new Identifier[]{new Identifier("container/enchanting_table/level_1_disabled"), new Identifier("container/enchanting_table/level_2_disabled"), new Identifier("container/enchanting_table/level_3_disabled")};
+        power = Math.min((int)Math.ceil(power/10.0)-1, 2);
+        Identifier[] TEXTURES_REORDERED = new Identifier[3];
+        for (int i = 0;i<3;i++) TEXTURES_REORDERED[i] = TEXTURES[power];
+        return TEXTURES_REORDERED;
+    }
 
 }
