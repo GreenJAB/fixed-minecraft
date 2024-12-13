eba42cdb3e981d5231a5cfac9f89544db0984171
joshua <joshua@baikie.org>
2024-12-14 01:09:38 +1100

    fixed bug with infintely enchantable horse armour

--- src/main/java/net/greenjab/fixedminecraft/mixin/horse/ItemStackMixin.java
+++ src/main/java/net/greenjab/fixedminecraft/mixin/horse/ItemStackMixin.java
@@ -3,17 +3,21 @@ package net.greenjab.fixedminecraft.mixin.horse;
 import net.minecraft.item.HorseArmorItem;
 import net.minecraft.item.ItemStack;
 import org.spongepowered.asm.mixin.Mixin;
+import org.spongepowered.asm.mixin.Shadow;
 import org.spongepowered.asm.mixin.injection.At;
 import org.spongepowered.asm.mixin.injection.Inject;
 import org.spongepowered.asm.mixin.injection.callback.CallbackInfoReturnable;
 
 @Mixin(ItemStack.class)
-public class ItemStackMixin {
+public abstract class ItemStackMixin {
+
+    @Shadow
+    public abstract boolean hasEnchantments();
 
     @Inject(method = "isEnchantable", at = @At("HEAD"), cancellable = true)
     private void enchantableHorseArmor(CallbackInfoReturnable<Boolean> cir) {
         if (((ItemStack)(Object)this).getItem() instanceof HorseArmorItem) {
-            cir.setReturnValue(true);
+            cir.setReturnValue(!this.hasEnchantments());
         }
     }
 }
