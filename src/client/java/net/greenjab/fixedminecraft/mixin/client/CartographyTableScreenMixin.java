f406019cf4e31c6a15cac9bd90e9c7fd76f0da0c
joshua <joshua@baikie.org>
2025-05-10 18:05:41 +1000

    More fixes to chest loot tables mapbook doesn't show a map in cartography table slightly increased some structure gen

---
+++ src/client/java/net/greenjab/fixedminecraft/mixin/client/CartographyTableScreenMixin.java
@@ -0,0 +1,21 @@
+package net.greenjab.fixedminecraft.mixin.client;
+
+import net.greenjab.fixedminecraft.registry.item.map_book.MapBookItem;
+import net.minecraft.client.gui.screen.ingame.CartographyTableScreen;
+import net.minecraft.component.ComponentType;
+import net.minecraft.item.ItemStack;
+import org.spongepowered.asm.mixin.Mixin;
+import org.spongepowered.asm.mixin.injection.At;
+import org.spongepowered.asm.mixin.injection.Redirect;
+
+@Mixin(CartographyTableScreen.class)
+public class CartographyTableScreenMixin {
+
+    @Redirect(method = "drawBackground", at = @At(value = "INVOKE", target = "Lnet/minecraft/item/ItemStack;get(Lnet/minecraft/component/ComponentType;)Ljava/lang/Object;"))
+    private Object mapBookDisplay(ItemStack itemStack, ComponentType componentType) {
+        if (itemStack.getItem() instanceof MapBookItem) {
+            return null;
+        }
+        return itemStack;
+    }
+}
