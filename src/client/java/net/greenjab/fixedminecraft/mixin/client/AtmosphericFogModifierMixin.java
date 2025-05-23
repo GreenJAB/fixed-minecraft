e4719477193ff802e1dad1137922b6d449c21d44
joshua <joshua@baikie.org>
2025-05-24 01:24:38 +1000

    More work on fog/pale fog

--- src/client/java/net/greenjab/fixedminecraft/mixin/client/AtmosphericFogModifierMixin.java
+++ src/client/java/net/greenjab/fixedminecraft/mixin/client/AtmosphericFogModifierMixin.java
@@ -1,5 +1,7 @@
 package net.greenjab.fixedminecraft.mixin.client;
 
+import net.greenjab.fixedminecraft.FixedMinecraftClient;
+import net.greenjab.fixedminecraft.registry.ModTags;
 import net.minecraft.client.render.RenderTickCounter;
 import net.minecraft.client.render.fog.AtmosphericFogModifier;
 import net.minecraft.client.render.fog.FogData;
@@ -17,12 +19,33 @@ import static org.joml.Math.lerp;
 public class AtmosphericFogModifierMixin {
 
     @Shadow
-    private float fogMultiplier;
+    public float fogMultiplier;
 
     @Inject(method = "applyStartEndModifier", at = @At(value = "TAIL"))
-    private void revertFog2(FogData data, Entity cameraEntity, BlockPos cameraPos, ClientWorld world, float viewDistance,
+    private void revertAndPaleFog(FogData data, Entity cameraEntity, BlockPos cameraPos, ClientWorld world, float viewDistance,
                            RenderTickCounter tickCounter, CallbackInfo ci){
         data.environmentalEnd = lerp(viewDistance, 1024.0F, this.fogMultiplier) + -256.0F * this.fogMultiplier;
         data.environmentalStart = lerp(viewDistance-Math.min(64f, viewDistance / 2), this.fogMultiplier * -160.0F, this.fogMultiplier);
+
+
+        float palefog = FixedMinecraftClient.paleGardenFog;
+        boolean inPale = world.getBiome(cameraEntity.getBlockPos()).isIn(ModTags.IS_PALE_GARDEN);
+        float delta = tickCounter.getDynamicDeltaTicks();
+        if (inPale) {
+            float f = (float) (delta * Math.sqrt(1-palefog*palefog)*0.01f);
+            FixedMinecraftClient.paleGardenFog = Math.min(palefog + f, 1);
+        } else {
+            FixedMinecraftClient.paleGardenFog = Math.max(palefog - delta*0.01f, 0);
+        }
+
+        if (palefog>0) {
+            float palefog2 = palefog * palefog;
+            float paleStart = 3 + (viewDistance - 3) * (1 - palefog2) / (75 * palefog2 + 1);
+            float paleEnd = 16 + (viewDistance - 16) * (1 - palefog2) / (25 * palefog2 + 1);
+            float circ = (float) Math.sqrt(1-(1-palefog)*(1-palefog));
+            data.environmentalEnd = lerp(data.environmentalEnd, paleEnd, circ);
+            data.environmentalStart = lerp(data.environmentalStart, paleStart, circ);
+        }
+
     }
 }
