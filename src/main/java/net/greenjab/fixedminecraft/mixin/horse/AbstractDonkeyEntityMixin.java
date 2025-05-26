03598ae4083249a4c8804fee8ccd2b06316fac5b
joshua <joshua@baikie.org>
2025-05-27 03:38:34 +1000

    Randomises donkey speed and jump height makes elytra function like netherite unbreakables nether maps work

--- src/main/java/net/greenjab/fixedminecraft/mixin/horse/AbstractDonkeyEntityMixin.java
+++ src/main/java/net/greenjab/fixedminecraft/mixin/horse/AbstractDonkeyEntityMixin.java
@@ -1,14 +1,20 @@
 package net.greenjab.fixedminecraft.mixin.horse;
 
 import net.minecraft.entity.EntityType;
+import net.minecraft.entity.attribute.EntityAttributeInstance;
+import net.minecraft.entity.attribute.EntityAttributes;
 import net.minecraft.entity.passive.AbstractDonkeyEntity;
 import net.minecraft.entity.passive.AbstractHorseEntity;
 import net.minecraft.entity.passive.MuleEntity;
+import net.minecraft.util.math.random.Random;
 import net.minecraft.world.World;
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.injection.At;
 import org.spongepowered.asm.mixin.injection.Inject;
+import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
 import org.spongepowered.asm.mixin.injection.callback.CallbackInfoReturnable;
+
+import java.util.Objects;
 
 @Mixin(AbstractDonkeyEntity.class)
 public abstract class AbstractDonkeyEntityMixin extends AbstractHorseEntity {
@@ -23,5 +29,18 @@ public abstract class AbstractDonkeyEntityMixin extends AbstractHorseEntity {
             cir.setReturnValue(muleEntity.hasChest()?3:0);
             cir.cancel();
         }
+    }
+
+    @Inject(method = "initAttributes", at = @At(value = "TAIL"))
+    private void randomisedAttributes(Random random, CallbackInfo ci){
+        AbstractDonkeyEntity ADE = (AbstractDonkeyEntity)(Object)this;
+
+        EntityAttributeInstance var10000 = ADE.getAttributeInstance(EntityAttributes.JUMP_STRENGTH);
+        Objects.requireNonNull(random);
+        var10000.setBaseValue(getChildJumpStrengthBonus(random::nextDouble));
+
+        var10000 = ADE.getAttributeInstance(EntityAttributes.MOVEMENT_SPEED);
+        Objects.requireNonNull(random);
+        var10000.setBaseValue(getChildMovementSpeedBonus(random::nextDouble));
     }
 }
---
+++ src/main/java/net/greenjab/fixedminecraft/mixin/map_book/FilledMapItemMixin.java
@@ -0,0 +1,20 @@
+package net.greenjab.fixedminecraft.mixin.map_book;
+
+
+import com.llamalad7.mixinextras.injector.ModifyExpressionValue;
+import net.minecraft.item.FilledMapItem;
+import org.spongepowered.asm.mixin.Mixin;
+import org.spongepowered.asm.mixin.injection.At;
+
+@Mixin(FilledMapItem.class)
+public class FilledMapItemMixin {
+
+    @ModifyExpressionValue(
+            method = "updateColors",
+            at = @At(value = "INVOKE", target = "Lnet/minecraft/world/dimension/DimensionType;hasCeiling()Z")
+    )
+    private boolean updateNetherColours(boolean original) {
+        return false;
+    }
+
+}
