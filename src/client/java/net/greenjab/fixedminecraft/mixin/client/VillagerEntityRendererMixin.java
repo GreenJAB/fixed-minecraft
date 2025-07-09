7c524482d69e24bd392c910c2e39427789657cb5
joshua <joshua@baikie.org>
2025-07-10 00:02:45 +1000

    fixed villager armor rendering chainmail protection, horse double enchant prots

--- src/client/java/net/greenjab/fixedminecraft/mixin/client/VillagerEntityRendererMixin.java
+++ src/client/java/net/greenjab/fixedminecraft/mixin/client/VillagerEntityRendererMixin.java
@@ -1,18 +1,20 @@
 package net.greenjab.fixedminecraft.mixin.client;
 
-
-import net.greenjab.fixedminecraft.models.HumanArmorFeatureRenderer;
+import net.greenjab.fixedminecraft.models.HumanoidRenderState;
+import net.greenjab.fixedminecraft.models.VillagerArmorLayer;
 import net.greenjab.fixedminecraft.models.ModelLayers;
 import net.greenjab.fixedminecraft.models.VillagerArmorModel;
 import net.minecraft.client.render.entity.EntityRendererFactory;
 import net.minecraft.client.render.entity.VillagerEntityRenderer;
+import net.minecraft.client.render.entity.state.VillagerEntityRenderState;
+import net.minecraft.entity.EquipmentSlot;
+import net.minecraft.entity.passive.VillagerEntity;
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.injection.At;
 import org.spongepowered.asm.mixin.injection.Inject;
 import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
 
 /** Credit: Viola-Siemens */
-/** Disabled for now as I couldn't get it to work */
 @Mixin(VillagerEntityRenderer.class)
 public class VillagerEntityRendererMixin {
 
@@ -20,11 +22,20 @@ public class VillagerEntityRendererMixin {
     public void addVillagerArmorLayer(EntityRendererFactory.Context context, CallbackInfo ci) {
         VillagerEntityRenderer current = ((VillagerEntityRenderer)(Object)this);
 
-        /*current.addFeature(new HumanArmorFeatureRenderer(
+        current.addFeature(new VillagerArmorLayer(
                 current,
                 new VillagerArmorModel(context.getPart(ModelLayers.VILLAGER_INNER_ARMOR)),
                 new VillagerArmorModel(context.getPart(ModelLayers.VILLAGER_OUTER_ARMOR)),
                 context.getEquipmentRenderer()
-        ));*/
+        ));
+    }
+    @Inject(method = "updateRenderState(Lnet/minecraft/entity/passive/VillagerEntity;Lnet/minecraft/client/render/entity/state/VillagerEntityRenderState;F)V", at = @At(value = "TAIL"))
+    public void extractHumanoidRenderState(VillagerEntity villagerEntity, VillagerEntityRenderState villagerEntityRenderState, float f,
+                                           CallbackInfo ci) {
+        HumanoidRenderState humanoidRenderState = (HumanoidRenderState)villagerEntityRenderState;
+        humanoidRenderState.fixed$setHeadEquipment(villagerEntity.getEquippedStack(EquipmentSlot.HEAD).copy());
+        humanoidRenderState.fixed$setChestEquipment(villagerEntity.getEquippedStack(EquipmentSlot.CHEST).copy());
+        humanoidRenderState.fixed$setLegEquipment(villagerEntity.getEquippedStack(EquipmentSlot.LEGS).copy());
+        humanoidRenderState.fixed$setFeetEquipment(villagerEntity.getEquippedStack(EquipmentSlot.FEET).copy());
     }
 }
