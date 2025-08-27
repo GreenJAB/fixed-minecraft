1bfbb0e8e9ddf3c8511200005173d36ca0573bcd
joshua <joshua@baikie.org>
2025-08-27 15:23:53 +1000

    eat in peaceful elytra in easy floating islands

--- src/main/java/net/greenjab/fixedminecraft/mixin/food/PlayerEntityMixin.java
+++ src/main/java/net/greenjab/fixedminecraft/mixin/food/PlayerEntityMixin.java
@@ -1,12 +1,15 @@
 package net.greenjab.fixedminecraft.mixin.food;
 
+import com.llamalad7.mixinextras.sugar.Local;
 import net.greenjab.fixedminecraft.registry.registries.GameruleRegistry;
 import net.minecraft.entity.player.PlayerEntity;
 import net.minecraft.server.world.ServerWorld;
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.injection.At;
+import org.spongepowered.asm.mixin.injection.Inject;
 import org.spongepowered.asm.mixin.injection.ModifyVariable;
 import org.spongepowered.asm.mixin.injection.Redirect;
+import org.spongepowered.asm.mixin.injection.callback.CallbackInfoReturnable;
 
 @Mixin(PlayerEntity.class)
 public class PlayerEntityMixin
@@ -22,4 +25,10 @@ public class PlayerEntityMixin
 
     @Redirect(method = "attack", at = @At(value = "INVOKE", target = "Lnet/minecraft/entity/player/PlayerEntity;setSprinting(Z)V"))
     private void removeServerClientDesync(PlayerEntity instance, boolean b) {}
+
+    @Inject(method = "canConsume", at = @At("HEAD"), cancellable = true)
+    private void alwaysEatInPeaceful(boolean ignoreHunger, CallbackInfoReturnable<Boolean> cir) {
+        PlayerEntity PE = (PlayerEntity)(Object)this;
+        if (PE.getEntityWorld().getDifficulty().getId()==0) cir.setReturnValue(true);
+    }
 }
