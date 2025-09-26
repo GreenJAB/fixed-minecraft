d90992e7b4b7b1cf58e91986a7d163202aa6428f
joshua <joshua@baikie.org>
2025-09-26 22:39:24 +1000

    fixing last commit

--- src/main/java/net/greenjab/fixedminecraft/mixin/enchanting/ProjectileUtilMixin.java
+++ src/main/java/net/greenjab/fixedminecraft/mixin/enchanting/ProjectileUtilMixin.java
@@ -18,7 +18,7 @@ public class ProjectileUtilMixin {
 
     @Redirect(method = "getEntityCollision(Lnet/minecraft/world/World;Lnet/minecraft/entity/Entity;Lnet/minecraft/util/math/Vec3d;Lnet/minecraft/util/math/Vec3d;Lnet/minecraft/util/math/Box;Ljava/util/function/Predicate;F)Lnet/minecraft/util/hit/EntityHitResult;",
               at = @At(value = "INVOKE", target = "Lnet/minecraft/util/math/Box;raycast(Lnet/minecraft/util/math/Vec3d;Lnet/minecraft/util/math/Vec3d;)Ljava/util/Optional;"))
-    private static Optional<Vec3d> test2(Box instance, Vec3d from, Vec3d to,
+    private static Optional<Vec3d> collideIfAlreadyInHitbox(Box instance, Vec3d from, Vec3d to,
                                          @Local(argsOnly = true)Entity arrow, @Local(argsOnly = true) float margin) {
         if (arrow instanceof ProjectileEntity) {
             if (instance.contains(from) && arrow.age>2) {
@@ -29,9 +29,9 @@ public class ProjectileUtilMixin {
     }
 
     @ModifyExpressionValue(method = "getToleranceMargin", at = @At(value = "FIELD", target = "Lnet/minecraft/entity/Entity;age:I"))
-    private static int test4(int original, @Local(argsOnly = true)Entity arrow) {
+    private static int serverClientSync(int original, @Local(argsOnly = true)Entity arrow) {
         if (arrow instanceof ProjectileEntity) {
-            if (arrow.getEntityWorld().isClient()) {
+            if (arrow.getWorld().isClient()) {
                 return original-1;
             }
         }
