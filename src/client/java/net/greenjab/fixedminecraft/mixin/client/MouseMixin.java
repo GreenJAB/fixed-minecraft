28f2ebdaf47eb206284094e69b5f9fae4519b2d3
joshua <joshua@baikie.org>
2025-11-01 12:20:49 +1100

    hotbar scrolling
    hotbar scrolling p2

---
+++ src/client/java/net/greenjab/fixedminecraft/mixin/client/MouseMixin.java
@@ -0,0 +1,41 @@
+package net.greenjab.fixedminecraft.mixin.client;
+
+
+import com.llamalad7.mixinextras.sugar.Local;
+import net.greenjab.fixedminecraft.hud.HotbarCycler;
+import net.minecraft.client.MinecraftClient;
+import net.minecraft.client.Mouse;
+import net.minecraft.client.util.InputUtil;
+import net.minecraft.entity.player.PlayerInventory;
+import net.minecraft.util.math.Direction;
+import org.spongepowered.asm.mixin.Final;
+import org.spongepowered.asm.mixin.Mixin;
+import org.spongepowered.asm.mixin.Shadow;
+import org.spongepowered.asm.mixin.injection.At;
+import org.spongepowered.asm.mixin.injection.Inject;
+import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
+
+@Mixin(Mouse.class)
+public class MouseMixin {
+
+    @Shadow
+    @Final
+    private MinecraftClient client;
+
+    @Inject(method = "onMouseScroll", at = @At(value = "INVOKE",
+                                               target = "Lnet/minecraft/entity/player/PlayerInventory;setSelectedSlot(I)V"
+    ), cancellable = true)
+    public void addHotBarScoller(long window, double horizontal, double vertical, CallbackInfo ci, @Local PlayerInventory playerInventory) {
+
+        final Direction direction = Math.signum(vertical) > 0
+                ? Direction.UP : Direction.DOWN;
+        if (InputUtil.isKeyPressed(MinecraftClient.getInstance().getWindow().getHandle(), HotbarCycler.getCycleKeyBinding().getDefaultKey().getCode())){
+            if (client.player.isSneaking()) {
+                HotbarCycler.shiftRows(MinecraftClient.getInstance(), direction);
+            } else {
+                HotbarCycler.shiftSingle(MinecraftClient.getInstance(), playerInventory.getSelectedSlot(), direction);
+            }
+            ci.cancel();
+        }
+    }
+}
