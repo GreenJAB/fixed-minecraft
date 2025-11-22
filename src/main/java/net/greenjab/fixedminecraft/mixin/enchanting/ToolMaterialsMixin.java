187d4d274cddf22d0a389b31400496ac35cf72a2
joshua <joshua@baikie.org>
2025-11-22 22:11:34 +1100

    gold autorepairs (but less durability) buffed/fixed debris
    a

--- src/main/java/net/greenjab/fixedminecraft/mixin/enchanting/ToolMaterialsMixin.java
+++ src/main/java/net/greenjab/fixedminecraft/mixin/enchanting/ToolMaterialsMixin.java
@@ -17,8 +17,9 @@ public class ToolMaterialsMixin {
             )
     private static ToolMaterial goldToolsLastLonger(TagKey<Block> tagKey, int i, float f, float g, int j, TagKey<Block> tagKey2,
                                                      Operation<ToolMaterial> original) {
-        if (i == 32) {
-            return original.call(tagKey, 59, f, g, j, tagKey2);
+
+        if (i == 32) { //gold
+            return original.call(tagKey, 48, f, g, j, tagKey2);
         }
         return original.call(tagKey, i, f, g, j, tagKey2);
     }
