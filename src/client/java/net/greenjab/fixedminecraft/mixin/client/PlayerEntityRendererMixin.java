908cece7df041aee7fe0a337c1509b00a17f876f
joshua <joshua@baikie.org>
2025-12-16 22:48:05 +1100

    dispenser minecraft crafting
    reduced trident drowned throw interval added bait to fisherman
    
    eat food in 3rd person
    
    a
    
    b

---
+++ src/client/java/net/greenjab/fixedminecraft/mixin/client/PlayerEntityRendererMixin.java
@@ -0,0 +1,27 @@
+package net.greenjab.fixedminecraft.mixin.client;
+
+import net.minecraft.client.render.entity.PlayerEntityRenderer;
+import net.minecraft.client.render.entity.model.BipedEntityModel;
+import net.minecraft.entity.player.PlayerEntity;
+import net.minecraft.item.ItemStack;
+import net.minecraft.item.consume.UseAction;
+import net.minecraft.util.Hand;
+import org.spongepowered.asm.mixin.Mixin;
+import org.spongepowered.asm.mixin.injection.At;
+import org.spongepowered.asm.mixin.injection.Inject;
+import org.spongepowered.asm.mixin.injection.callback.CallbackInfoReturnable;
+
+@Mixin(PlayerEntityRenderer.class)
+public class PlayerEntityRendererMixin {
+
+    @Inject(method = "getArmPose(Lnet/minecraft/entity/player/PlayerEntity;Lnet/minecraft/item/ItemStack;Lnet/minecraft/util/Hand;)Lnet/minecraft/client/render/entity/model/BipedEntityModel$ArmPose;", at = @At(
+            value = "INVOKE",
+            target = "Lnet/minecraft/item/ItemStack;getUseAction()Lnet/minecraft/item/consume/UseAction;"
+    ), cancellable = true)
+    private static void test(PlayerEntity player, ItemStack stack, Hand hand, CallbackInfoReturnable<BipedEntityModel.ArmPose> cir){
+        UseAction useAction = stack.getUseAction();
+        if (useAction == UseAction.EAT || useAction == UseAction.DRINK) {
+            cir.setReturnValue(BipedEntityModel.ArmPose.TOOT_HORN);
+        }
+    }
+}
