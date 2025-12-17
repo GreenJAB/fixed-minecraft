cbcfe2f2ba9aa9c2b7c7cecb721570de6ff84168
joshua <joshua@baikie.org>
2025-12-18 08:05:43 +1100

    c

--- src/client/java/net/greenjab/fixedminecraft/mixin/client/PlayerEntityRendererMixin.java
+++ src/client/java/net/greenjab/fixedminecraft/mixin/client/PlayerEntityRendererMixin.java
@@ -1,11 +1,12 @@
 package net.greenjab.fixedminecraft.mixin.client;
 
+import net.minecraft.client.network.AbstractClientPlayerEntity;
 import net.minecraft.client.render.entity.PlayerEntityRenderer;
 import net.minecraft.client.render.entity.model.BipedEntityModel;
 import net.minecraft.entity.player.PlayerEntity;
 import net.minecraft.item.ItemStack;
-import net.minecraft.item.consume.UseAction;
 import net.minecraft.util.Hand;
+import net.minecraft.util.UseAction;
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.injection.At;
 import org.spongepowered.asm.mixin.injection.Inject;
@@ -14,12 +15,12 @@ import org.spongepowered.asm.mixin.injection.callback.CallbackInfoReturnable;
 @Mixin(PlayerEntityRenderer.class)
 public class PlayerEntityRendererMixin {
 
-    @Inject(method = "getArmPose(Lnet/minecraft/entity/player/PlayerEntity;Lnet/minecraft/item/ItemStack;Lnet/minecraft/util/Hand;)Lnet/minecraft/client/render/entity/model/BipedEntityModel$ArmPose;", at = @At(
+    @Inject(method = "getArmPose", at = @At(
             value = "INVOKE",
-            target = "Lnet/minecraft/item/ItemStack;getUseAction()Lnet/minecraft/item/consume/UseAction;"
+            target = "Lnet/minecraft/item/ItemStack;getUseAction()Lnet/minecraft/util/UseAction;"
     ), cancellable = true)
-    private static void test(PlayerEntity player, ItemStack stack, Hand hand, CallbackInfoReturnable<BipedEntityModel.ArmPose> cir){
-        UseAction useAction = stack.getUseAction();
+    private static void test(AbstractClientPlayerEntity player, Hand hand, CallbackInfoReturnable<BipedEntityModel.ArmPose> cir){
+        UseAction useAction =  player.getStackInHand(hand).getUseAction();
         if (useAction == UseAction.EAT || useAction == UseAction.DRINK) {
             cir.setReturnValue(BipedEntityModel.ArmPose.TOOT_HORN);
         }
