53e8c2f8548c5c348943d351a62653f6e847add5
joshua <joshua@baikie.org>
2025-12-21 23:28:45 +1100

    other minor additions before 1.6.1
    minor villager trade fixes
    
    a
    
    b
    
    c

--- src/main/java/net/greenjab/fixedminecraft/mixin/food/HungerManagerMixin.java
+++ src/main/java/net/greenjab/fixedminecraft/mixin/food/HungerManagerMixin.java
@@ -125,8 +125,7 @@ public abstract class HungerManagerMixin {
     private boolean needSaturationToHeal(PlayerEntity instance) {
         HungerManager HM = (HungerManager) (Object)this;
         if (instance.hurtTime>0) return false;
-
-        return instance.canFoodHeal() && HM.getSaturationLevel()>3 &&
+        return instance.canFoodHeal() && (instance.getHealth() <= instance.getMaxHealth()-1) && HM.getSaturationLevel()>3 &&
                (HM.getSaturationLevel()>=HM.getFoodLevel() || instance.isSneaking());
     }
     @ModifyArg(method = "update", at = @At(value = "INVOKE", target = "Lnet/minecraft/entity/player/HungerManager;addExhaustion(F)V"), index = 0)
