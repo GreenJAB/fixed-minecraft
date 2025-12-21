58401921fde8117780cb33c8cb9b8e98e2173ea6
joshua <joshua@baikie.org>
2025-12-21 23:28:45 +1100

    other minor additions before 1.6.1
    minor villager trade fixes
    
    a

--- src/main/java/net/greenjab/fixedminecraft/mixin/phantom/PhantomSpawnerMixin.java
+++ src/main/java/net/greenjab/fixedminecraft/mixin/phantom/PhantomSpawnerMixin.java
@@ -32,7 +32,7 @@ public class PhantomSpawnerMixin {
     private int phantomSpawnByEffect(ServerStatHandler instance, Stat<?> stat,
                                      @Local ServerPlayerEntity serverPlayerEntity) {
         if (!serverPlayerEntity.hasStatusEffect(StatusRegistry.INSOMNIA)) return 0;
-        return 720000;
+        return 100000 * (1+serverPlayerEntity.getStatusEffect(StatusRegistry.INSOMNIA).getAmplifier());
     }
 
     @Redirect(method = "spawn", at = @At(value = "INVOKE", target = "Lnet/minecraft/world/Difficulty;getId()I"))
@@ -41,7 +41,7 @@ public class PhantomSpawnerMixin {
                                      @Local LocalDifficulty localDifficulty,
                                      @Local Random random) {
         if (!serverPlayerEntity.hasStatusEffect(StatusRegistry.INSOMNIA)) return 0;
-        return instance.getId() + serverPlayerEntity.getStatusEffect(StatusRegistry.INSOMNIA).getAmplifier()*2;
+        return instance.getId() + serverPlayerEntity.getStatusEffect(StatusRegistry.INSOMNIA).getAmplifier();
     }
 
     @Inject(method = "spawn", at = @At(value = "INVOKE", target = "Lnet/minecraft/util/math/MathHelper;clamp(III)I", shift = At.Shift.AFTER))
