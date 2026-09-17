934f6d513ce3d2ed4c6f4023d7901fc2cd9cee96
joshua <joshua@baikie.org>
2026-09-17 20:05:46 +1000

    split wavy stuff into own resource pack end fire

--- src/main/java/net/greenjab/fixedminecraft/mixin/other/SpawnArmorTrimsCommandMixin.java
+++ src/main/java/net/greenjab/fixedminecraft/mixin/other/SpawnArmorTrimsCommandMixin.java
@@ -1,20 +1,24 @@
 package net.greenjab.fixedminecraft.mixin.other;
 
+import com.llamalad7.mixinextras.injector.wrapoperation.Operation;
+import com.llamalad7.mixinextras.injector.wrapoperation.WrapOperation;
 import net.greenjab.fixedminecraft.registry.registries.TrimMaterialsRegistry;
 import net.minecraft.resources.ResourceKey;
 import net.minecraft.server.commands.SpawnArmorTrimsCommand;
 import net.minecraft.world.item.equipment.trim.TrimMaterial;
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.injection.At;
-import org.spongepowered.asm.mixin.injection.ModifyArg;
+import java.util.ArrayList;
 import java.util.List;
+import java.util.function.ToIntFunction;
 
 @Mixin(SpawnArmorTrimsCommand.class)
 public abstract class SpawnArmorTrimsCommandMixin {
 
-    @ModifyArg(method = "<clinit>", at = @At(value = "INVOKE", target = "Lnet/minecraft/util/Util;createIndexLookup(Ljava/util/List;)Ljava/util/function/ToIntFunction;", ordinal = 1))
-    private static <T extends ResourceKey<TrimMaterial>> List<T> addCoalTrim(List<T> values) {
-        values.add((T)TrimMaterialsRegistry.COAL);
-        return values;
+    @WrapOperation(method = "<clinit>", at = @At(value = "INVOKE", target = "Lnet/minecraft/util/Util;createIndexLookup(Ljava/util/List;)Ljava/util/function/ToIntFunction;", ordinal = 1))
+    private static <T extends ResourceKey<TrimMaterial>> ToIntFunction<T> addCoalTrim(List<T> values, Operation<ToIntFunction<T>> original) {
+        ArrayList<T> newBlocks = new ArrayList<>(values);
+        newBlocks.add((T)TrimMaterialsRegistry.COAL);
+        return original.call(newBlocks);
     }
 }
--- src/main/java/net/greenjab/fixedminecraft/registry/registries/BlockRegistry.java
+++ src/main/java/net/greenjab/fixedminecraft/registry/registries/BlockRegistry.java
@@ -4,6 +4,7 @@ package net.greenjab.fixedminecraft.registry.registries;
 import net.greenjab.fixedminecraft.FixedMinecraft;
 import net.greenjab.fixedminecraft.registry.block.CopperFireBlock;
 import net.greenjab.fixedminecraft.registry.block.CopperRailBlock;
+import net.greenjab.fixedminecraft.registry.block.EndFireBlock;
 import net.greenjab.fixedminecraft.registry.block.NetheriteAnvilBlock;
 import net.greenjab.fixedminecraft.registry.block.OxidizableRailBlock;
 import net.greenjab.fixedminecraft.registry.block.RedstoneLanternBlock;
@@ -215,6 +216,19 @@ public class BlockRegistry {
                     .lightLevel(_ -> 10)
                     .sound(SoundType.WOOL)
                     .pushReaction(PushReaction.POPPED)
+    );
+
+    public static final Block END_FIRE = register(
+            "end_fire",
+            EndFireBlock::new,
+            BlockBehaviour.Properties.of()
+                    .mapColor(MapColor.COLOR_PINK)
+                    .replaceable()
+                    .noCollision()
+                    .instabreak()
+                    .lightLevel(_ -> 10)
+                    .sound(SoundType.WOOL)
+                    .pushReaction(PushReaction.DESTROY)
     );
 
     public static final Block REDSTONE_LANTERN = register(
